665f0f0e98fdb206c63e593efdc7974fa11299d9
Trevor Murphy <murphtrevor@gmail.com>
2017-12-24 19:51:38 -0600

    Cleanup code

--- functions/index.js
+++ functions/index.js
@@ -8,40 +8,24 @@ const accountSid = config.account_sid;
 const authToken = config.auth_token;
 const client = require('twilio')(accountSid, authToken);
 
-
 exports.addMessage = functions.https.onRequest((req, res) => {
-  console.log('Entering test function...');
   const app = new ActionsSdkApp({request: req, response: res});
 
   function mainIntent(app) {
-    console.log('mainIntent');
-    let inputPrompt = app.buildInputPrompt(false, 'Say something bitch.');
-    app.ask(inputPrompt);
-  }
-
-  function rawInput(app) {
-    console.log('rawInput');
-    if (app.getRawInput() === 'bye') {
-      app.tell('Fuck off...');
-    } else {
-      let inputPrompt = app.buildInputPrompt(false, 'You just said ' +
-        app.getRawInput() + '. Say something else I dare ya.');
-      const to_addresses = config.to.split(',');
-      to_addresses.forEach((to) => {
-        client.messages.create({
-            body: app.getRawInput(),
-            to: config.to.split(','),  // Text this number
-            from: config.from // From a valid Twilio number
-        })
-        .then((message) => console.log(message.sid));
-      });
-      app.ask(inputPrompt);
-    }
+    console.log('Entering Main Intent');
+    const to_addresses = config.to.split(',');
+    to_addresses.forEach((to) => {
+      client.messages.create({
+          body: app.getRawInput(),
+          to: to,  // Text this number
+          from: config.from // From a valid Twilio number
+      })
+      .then((message) => console.log(message.sid));
+    });
+    app.tell('Messages sent :)');
   }
 
   let actionMap = new Map();
   actionMap.set(app.StandardIntents.MAIN, mainIntent);
-  actionMap.set(app.StandardIntents.TEXT, rawInput);
-
   app.handleRequest(actionMap);
 });
